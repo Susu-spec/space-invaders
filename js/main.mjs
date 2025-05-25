25c43e1b2a9ae5c718912d9710ecc7a32ad87a63
Susu-spec <suleimansuwaibat@gmail.com>
2025-05-25 19:37:33 +0100

    feat: add particles and level update functions

--- js/main.mjs
+++ js/main.mjs
@@ -1,6 +1,6 @@
 import * as Utils from './utils.mjs';
 
-const { clamp, isColliding } = Utils;
+const { clamp, handlePlayerBulletsAlienCollision, handleAliensPlayerCollision, triggerGameOver, handleAlienLasersPlayerCollision } = Utils;
 
 const CANVAS_WIDTH = window.innerWidth;
 const CANVAS_HEIGHT = window.innerHeight;
@@ -10,9 +10,15 @@ const CENTER_Y = CANVAS_HEIGHT / 2;
 const ZOOM_DURATION = 1;
 const MAX_ZOOM = 1.5;
 
+const gameOverScreen = document.getElementById('game-over-screen');
+const scoreTracker = document.getElementById('score');
+const playAgain = document.getElementById('play-again-link');
+const startScreen = document.getElementById('start-screen');
+
+
 let keys = {};
 let gameStarted = false;
-var alienLasers = [];
+var spacing = 30;
 
 const createImage = (src) => {
   const img = new Image();
@@ -50,6 +56,7 @@ const Assets = {
     shoot: new Audio('./assets/sounds/shoot.wav'),
     playerDead: new Audio('./assets/sounds/player-dead.wav'),
     invaderDead: new Audio('./assets/sounds/invader-killed.wav'),
+    gameOverSound: new Audio('./assets/sounds/game-over.wav'),
     background: new Audio('./assets/sounds/space-invaders-background.mpeg')
   },
   clipRect: {
@@ -67,24 +74,25 @@ const levels = [
     name: 'Beginner',
     speedMultiplier: 1,
     alienCount: 5,
-    alienSpeed: 50,
+    alienSpeed: 30,
     duration: 30,
+    spacing: 35,
   },
   {
     id: 2,
     name: 'Intermediate',
     speedMultiplier: 1.5,
     alienCount: 8,
-    alienSpeed: 80,
-    duration: 45,
+    alienSpeed: 50,
+    spacing: 40,
   },
   {
     id: 3,
     name: 'Advanced',
     speedMultiplier: 2,
     alienCount: 12,
-    alienSpeed: 110,
-    duration: 60,
+    alienSpeed: 80,
+    spacing: 45,
   },
 ];
 
@@ -146,10 +154,10 @@ class GameEntity {
 
   updateBounding() {
     this.bounding.setNewBoundRect(
-    this.position.x, 
-    this.position.y, 
-    (this.clipRect.width * this.scale.x), 
-    (this.clipRect.height * this.scale.y)
+      this.position.x,
+      this.position.y,
+      (this.clipRect.width * this.scale.x),
+      (this.clipRect.height * this.scale.y)
     )
   }
 
@@ -184,21 +192,21 @@ class AnimatedGameEntity extends GameEntity {
     ctx.save();
     ctx.transform(
       this.scale.x,
-      0, 0, 
-      this.scale.y, 
-      this.position.x, 
+      0, 0,
+      this.scale.y,
+      this.position.x,
       this.position.y
     );
     // centers 
     ctx.drawImage(
-      this.img, 
-      this.clipRect.x, 
-      this.clipRect.y, 
-      this.clipRect.width, 
-      this.clipRect.height, 
-      Math.round(-this.clipRect.width*0.5), 
-      Math.round(-this.clipRect.height*0.5), 
-      this.clipRect.width, 
+      this.img,
+      this.clipRect.x,
+      this.clipRect.y,
+      this.clipRect.width,
+      this.clipRect.height,
+      Math.round(-this.clipRect.width * 0.5),
+      Math.round(-this.clipRect.height * 0.5),
+      this.clipRect.width,
       this.clipRect.height
     );
     ctx.restore();
@@ -208,7 +216,7 @@ class AnimatedGameEntity extends GameEntity {
 
 class Player extends AnimatedGameEntity {
   constructor(img) {
-    super(img, CANVAS_WIDTH/2, CANVAS_HEIGHT - (clipRect.player.height / 2) - 10, clipRect.player)
+    super(img, CANVAS_WIDTH / 2, CANVAS_HEIGHT - (clipRect.player.height / 2) - 10, clipRect.player)
     this.img = img;
     this.clipRect.width = this.img.width;
     this.clipRect.height = this.img.height;
@@ -219,12 +227,14 @@ class Player extends AnimatedGameEntity {
     this.lastShotTime = 0;
     this.dying = false;
     this.deathTimer = 0.5;
+    this.invincible = false;
+    this.invincibleTimer = 0;
   }
 
   reset() {
     this.lives = 3;
     this.score = 0;
-    this.position.set(CANVAS_WIDTH/2, CANVAS_HEIGHT -  (clipRect.player.height / 2) - 10);
+    this.position.set(CANVAS_WIDTH / 2, CANVAS_HEIGHT - (clipRect.player.height / 2) - 10);
     this.bullets = []
     this.img = images.player;
   }
@@ -238,7 +248,15 @@ class Player extends AnimatedGameEntity {
       }
       return;
     }
-    
+
+    if (this.invincible) {
+      this.invincibleTimer -= dt;
+      if (this.invincibleTimer <= 0) {
+        this.invincible = false;
+        this.invincibleTimer = 0;
+      }
+    }
+
     this.position.x = clamp(this.position.x, 50, CANVAS_WIDTH - this.img.width);
 
     if (keys['ArrowLeft']) {
@@ -246,7 +264,7 @@ class Player extends AnimatedGameEntity {
     } else if (keys['ArrowRight']) {
       this.position.x += this.xAccel * dt
     }
-    
+
     this.updateBounding();
   }
 
@@ -279,11 +297,11 @@ class Player extends AnimatedGameEntity {
     }
     this.bullets = this.bullets.filter(b => b.alive);
   }
-  
+
   drawBullets() {
     for (let bullet of this.bullets) {
       if (bullet.alive) {
-        bullet.drawEntityOnCanvas(); 
+        bullet.drawEntityOnCanvas();
       }
     }
   }
@@ -343,7 +361,7 @@ class Alien extends AnimatedGameEntity {
     const laserX = this.position.x + clipRect.alien.width / 2;
     const laserY = this.position.y + clipRect.alien.height;
     const laser = new Laser(laserX, laserY, -1, 800);
-    alienLasers.push(laser);
+    game.alienLasers.push(laser);
   }
 
   drawEntityOnCanvas() {
@@ -353,15 +371,15 @@ class Alien extends AnimatedGameEntity {
     ctx.globalAlpha = 0.01;
     ctx.restore();
   }
-}  
+}
 
 class AlienGrid {
-  constructor(x, y, rows, cols, spacing) {
+  constructor(x, y, rows, cols) {
     this.aliens = [];
     this.position = new Position(x, y + 10)
     this.rows = rows;
     this.cols = cols;
-    this.spacing = spacing;
+    this.spacing = 35;
     this.direction = 1;
     this.speed = 20;
     this.stepDown = 10;
@@ -377,7 +395,7 @@ class AlienGrid {
         const points = ALIEN_POINTS[row % ALIEN_POINTS.length];
 
         this.aliens.push(new Alien(img, x, y, points));
-        
+
       }
     }
   }
@@ -386,25 +404,36 @@ class AlienGrid {
     this.speed = speed;
   }
 
+  updateSpacing(spacing) {
+    this.spacing = spacing;
+    for (let i = 0; i < this.aliens.length; i++) {
+      const row = Math.floor(i / this.cols);
+      const col = i % this.cols;
+      this.aliens[i].position.x = this.position.x + col * this.spacing;
+          
+    }
+  }
+
   updateShooting(currentTime, dt) {
     var columnAliens = null;
     for (let col = 0; col < this.cols; col++) {
-      columnAliens = this.aliens.filter(alien => 
+      columnAliens = this.aliens.filter(alien =>
         Math.round((alien.position.x - this.position.x) / this.spacing) === col && alien.alive
       )
 
       const shooter = columnAliens.sort((a, b) => b.position.y - a.position.y)[0]
+      const randomiser = (this.currentLevel > 1) ? 0.01 : 0.001;
 
-      if (shooter && ((currentTime - shooter.lastShotTime) > shooter.shootCoolDown) && Math.random() < 0.001) {
+      if (shooter && ((currentTime - shooter.lastShotTime) > shooter.shootCoolDown) && Math.random() < randomiser) {
         shooter.shoot();
         shooter.lastShotTime = currentTime;
         shooter.shootCoolDown = Math.random() * 2000 + 1000;
       }
     }
 
-    for (let laser of alienLasers) {
+    for (let laser of game.alienLasers) {
       laser.movement(dt);
-    } 
+    }
   }
 
   update(currentTime, dt) {
@@ -416,8 +445,8 @@ class AlienGrid {
       if (alien.alive === false) continue;
 
       const nextXPosition = alien.position.x + xVelocity;
-      
-      if (nextXPosition <= 50 ||  nextXPosition + alien.img.width >= CANVAS_WIDTH - 50) {
+
+      if (nextXPosition <= 50 || nextXPosition + alien.img.width >= CANVAS_WIDTH - 50) {
         shouldStepDown = true;
         this.direction *= -1;
         break;
@@ -430,13 +459,14 @@ class AlienGrid {
       if (alien.dying) {
         alien.deathTimer -= dt;
         if (alien.deathTimer <= 0) {
+          game.particles.createExplosion(alien.position.x, alien.position.y);
           sounds.invaderDead.currentTime = 0;
           sounds.invaderDead.play();
           alien.alive = false;
           alien.dying = false;
         }
       }
-    
+
 
       if (shouldStepDown) {
         alien.position.y += this.stepDown;
@@ -459,6 +489,62 @@ class AlienGrid {
   }
 }
 
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.vx = (Math.random() - 0.5) * 4;
+    this.vy = (Math.random() - 0.5) * 4;
+    this.alpha = 1;
+    this.size = Math.random() * 3 + 2;
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 0.02;
+  }
+
+  draw(ctx) {
+    ctx.globalAlpha = this.alpha;
+    ctx.fillStyle = 'orange';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.globalAlpha = 1;
+  }
+
+  isAlive() {
+    return this.alpha > 0;
+  }
+}
+
+class Particles {
+  constructor() {
+    this.particles = [];
+  }
+
+  createExplosion(x, y) {
+    for (let i = 0; i < 20; i++) {
+      this.particles.push(new Particle(x, y));
+    }
+  }
+
+  updateParticles() {
+    this.particles = this.particles.filter(p => p.isAlive());
+    for (let p of this.particles) {
+      p.update();
+    }
+  }
+
+  drawParticles(ctx) {
+    for (let p of this.particles) {
+      p.draw(ctx);
+    }
+  }
+}
+
+
 class Game {
   constructor(canvas) {
     this.canvas = canvas;
@@ -466,32 +552,46 @@ class Game {
     this.lastTime = 0;
     this.player = null;
     this.aliens = null;
+    this.alienLasers = [];
     this.canShoot = true;
     this.currentLevel = 1;
     this.levelTimer = 0;
     this.zooming = false;
     this.zoomTimer = 0;
     this.zoomLevel = 1;
+    this.particles = null;
+
   }
 
   init() {
+    startScreen.classList.add('visible');
     this.canvas.width = CANVAS_WIDTH;
     this.canvas.height = CANVAS_HEIGHT;
+
     this.player = new Player(images.player);
-    this.aliens = new AlienGrid(50, 0, 5, 15, 40);
-    requestAnimationFrame(this.loop.bind(this));
+    this.aliens = new AlienGrid(50, 40, 6, 18);
+    this.particles = new Particles();
+    requestAnimationFrame(this.start.bind(this));
   }
 
   update(timeStamp, dt) {
     if (!gameStarted && this.state !== GameStates.PLAYING) return;
 
+    if (this.player.lives === 0 && this.state !== GameStates.GAME_OVER) {
+      this.setState(GameStates.GAME_OVER)
+      this.gameOver();
+    }
+
     if (this.state == GameStates.PLAYING) {
       this.player.movement(dt);
       this.player.updateBullets(dt)
-      this.player.movement(dt);
       this.aliens.update(timeStamp, dt);
       this.collisionDetection(dt);
       this.updateLevel(dt);
+      if (this.zooming) {
+        this.updateZooming(dt);
+      }
+      this.particles.updateParticles();
     }
   }
 
@@ -499,46 +599,37 @@ class Game {
     this.clearScreen();
     if (this.state === GameStates.LOADING) return;
 
-    const startScreen = document.getElementById('start-screen');
-    startScreen.classList.remove('visible');  
-
-    if (this.state === GameStates.PLAYING) {
-      sounds.background.loop = true;
-      sounds.background.play();
-
-      if (this.zooming) {
-        ctx.save();
-        ctx.translate(CENTER_X, CENTER_Y)
-        ctx.scale(this.zoomLevel, this.zoomLevel);
-        ctx.translate(-CENTER_X, -CENTER_Y)
-      }
-    }
-
-    this.player.drawEntityOnCanvas();  
+    this.hideStartScreen();
+    this.handleZoomStart();
+    this.player.drawEntityOnCanvas();
     this.player.drawBullets();
     this.aliens.drawAliensOnCanvas();
-    for (let bullet of alienLasers) {
+
+    for (let bullet of this.alienLasers) {
       bullet.drawEntityOnCanvas();
     }
 
+    for (let i = 0; i < this.player.lives; i++) {
+      const xOffset = 20 + i * 40;
+      const yOffset = 20;
+      ctx.drawImage(images.player, xOffset, yOffset, 25, 20);
+    }
+
+    this.drawScore();
+    this.particles.drawParticles(ctx);
     if (this.zooming && this.state === GameStates.PLAYING) {
       ctx.restore();
     }
   }
 
-  loop() {
+  start() {
     const timeStamp = performance.now();
     const deltaTime = (timeStamp - this.lastTime) / 1000;
+
     this.lastTime = timeStamp;
-
     this.update(timeStamp, deltaTime);
-
-    if (this.zooming) {
-      this.updateZooming(deltaTime); 
-    }
-
     this.draw();
-    requestAnimationFrame(this.loop.bind(this));
+    requestAnimationFrame(this.start.bind(this));
   }
 
   pause() {
@@ -579,29 +670,49 @@ class Game {
   }
 
   handleInput(e) {
+    const now = performance.now();
 
-    if (e.code === 'Escape') {
-      this.pause();
-      return;
+    switch (e.code) {
+      case 'Escape':
+        this.pause();
+        return;
+
+      case 'Enter':
+        if (this.state !== GameStates.PAUSED) {
+          gameStarted = true;
+          this.setState(GameStates.PLAYING);
+        }
+        break;
+
+      case 'Space':
+        if (
+          this.canShoot &&
+          this.player &&
+          this.state === GameStates.PLAYING &&
+          now - this.player.lastShotTime >= 300
+        ) {
+          this.player.shoot();
+          sounds.shoot.currentTime = 0;
+          sounds.shoot.play();
+          this.player.lastShotTime = now;
+          this.canShoot = false;
+        }
+        break;
     }
+  }
 
-    if (e.code === 'Enter' && this.state !== GameStates.PAUSED) {
-        gameStarted = true;
-        this.setState(GameStates.PLAYING)
-        this.zooming = true;
-    }
+  hideStartScreen() {
+    startScreen.classList.remove('visible');
+  }
 
-    if (e.code === "Space" && this.canShoot && this.player && this.state === GameStates.PLAYING) {
-      const now = performance.now();
+  drawScore() {
+    const gradient = ctx.createLinearGradient(CANVAS_WIDTH - 200, 40, (CANVAS_WIDTH - 200) + 150, 40);
+    gradient.addColorStop(0, "#037070");
+    gradient.addColorStop(1, '#00ffff');
 
-      if (now - this.player.lastShotTime >= 300) {
-        this.player.shoot();
-        sounds.shoot.currentTime = 0;
-        sounds.shoot.play();
-        this.player.lastShotTime = now;
-        this.canShoot = false;
-      }
-    }
+    ctx.font = "1rem 'Press Start 2P', monospace";
+    ctx.fillStyle = gradient;
+    ctx.fillText(`Score: ${this.player.score}`, CANVAS_WIDTH - 200, 40);
   }
 
   handleKeyDown(e) {
@@ -619,95 +730,101 @@ class Game {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   }
 
+  handleZoomStart() {
+    if (this.state === GameStates.PLAYING) {
+      sounds.background.loop = true;
+      sounds.background.play();
+
+      if (this.zooming) {
+        ctx.save();
+        ctx.translate(CENTER_X, CENTER_Y);
+        ctx.scale(this.zoomLevel, this.zoomLevel);
+        ctx.translate(-CENTER_X, -CENTER_Y);
+      }
+    }
+  }
+
   updateZooming(dt) {
+    const levelUpScreen = document.getElementById('level-up-screen');
+
     this.zoomTimer += dt;
 
     if (this.zoomTimer >= ZOOM_DURATION) {
       this.zooming = false;
       this.zoomLevel = 1;
+      this.zoomTimer = 0;
+      levelUpScreen.classList.remove('visible')
     } else {
       const t = this.zoomTimer / ZOOM_DURATION;
       this.zoomLevel = 1 + (MAX_ZOOM - 1) * (1 - Math.pow(1 - t, 3));
+      levelUpScreen.classList.add('visible');
     }
   }
 
-  updateLevel(dt) {
-    this.levelTimer += dt;
+  updateLevel() {
     const level = levels[this.currentLevel];
-    this.aliens.setSpeed(level.alienSpeed);
+    const CONSTANT_SCORE = 500;
+    const expectedScore = this.currentLevel * CONSTANT_SCORE;
+
+    if (
+      this.player.score >= expectedScore &&
+      this.currentLevel < levels.length
+    ) {
+      this.currentLevel++;
+      this.zooming = true;
   
-    // if (this.levelTimer >= level.duration) {
-    //   if (this.currentLevel < levels.length - 1) {
-    //     this.currentLevel++;
-    //     this.levelTimer = 0;
-    //   }
-    // }
-  }
-
-  checkPlayerBulletsVsAliens() {
-    // uses let .. of ... to return objects
-    // let ... in ... returns indices
-    for (let bullet of this.player.bullets) {
-      for (let alien of this.aliens.aliens) {
-        if (bullet.alive && alien.alive && isColliding(bullet, alien)) {
-          this.player.score += alien.points;
-
-          alien.dying = true;
-          alien.deathTimer = 0.5;
-          alien.img = images.enemyDeath;
-          bullet.alive = false;
-        }
-      }
+      this.aliens.setSpeed(this.aliens.speed + level.alienSpeed);
+      this.aliens.updateSpacing(level.spacing);
     }
   }
   
-  
-  checkAlienBulletsVsPlayer() {
-    if (this.player.lives <= 0) return;
 
-    for (let laser of alienLasers) {
-      if (this.player.lives > 0 && isColliding(laser, this.player)) {
-        sounds.playerDead.currentTime = 0;
-        sounds.playerDead.play();
 
-        this.player.lives -= 1;
-        this.player.dying = true;
-        this.player.deathTimer = 0.5;
-        this.player.img = images.playerDeath;
-
-        laser.alive = false;
-        break;
-      } 
-    }
-  }
-  
   checkPlayerVsAliens() {
-    for (let alien of this.aliens.aliens) {
-      const collided = isColliding(alien, this.player);
-      if (collided) {
-        this.gameOver();
-      }
+    let aliens = this.aliens.aliens;
+    let playerHeight = clipRect.player.height - 70;
+
+    let lowestY = handleAliensPlayerCollision(aliens);
+
+    if (lowestY >= (CANVAS_HEIGHT + playerHeight)) {
+      triggerGameOver(game, sounds);
     }
   }
 
-  collisionDetection(dt) {
-    this.checkPlayerBulletsVsAliens(dt);
-    this.checkAlienBulletsVsPlayer();
+
+  collisionDetection() {
+    handlePlayerBulletsAlienCollision(this.player.bullets, this.aliens.aliens, this.player, images.enemyDeath);
+    handleAlienLasersPlayerCollision(this.alienLasers, this.player, sounds.playerDead, images.playerDeath);
     this.checkPlayerVsAliens();
   }
 
   gameOver() {
-    const gameOverScreen = document.getElementById('game-over-screen');
-    const scoreTracker = document.getElementById('score');
 
     gameOverScreen.classList.add('visible');
-    scoreTracker.innerHTML += this.player.score;
-    
+    scoreTracker.innerHTML = `Your Score: ${this.player.score}`;
 
-  //  reset should only happen after you click on a button
+    sounds.background.pause();
+    sounds.background.currentTime = 0;
+
+ 
+    sounds.gameOverSound.play();
+    playAgain.onclick = () => this.reset();
+
+    //  reset should only happen after you click on a button
   }
 
   reset() {
+    const loading = document.getElementById('loading-screen');
+  
+    gameOverScreen.classList.remove('visible');
+    loading.classList.add('visible');
+  
+    setTimeout(() => {
+ 
+   
+    loading.classList.remove('visible');
+
+    // Reset game state
     this.state = GameStates.LOADING;
     this.lastTime = 0;
     this.player = null;
@@ -718,8 +835,12 @@ class Game {
     this.zooming = false;
     this.zoomTimer = 0;
     this.zoomLevel = 1;
-    alienLasers = [];
+    this.alienLasers = [];
+
+    this.init();
+    }, 500); 
   }
+  
 }
 
 const canvas = document.getElementById('game-screen');
@@ -727,7 +848,7 @@ const game = new Game(canvas);
 var ctx = canvas.getContext('2d');
 
 window.onload = () => game.init();
-document.addEventListener('keydown', (e) => { 
+document.addEventListener('keydown', (e) => {
   game.handleInput(e);
   game.handleKeyDown(e);
 });
